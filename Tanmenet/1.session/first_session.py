fb62f429cc795ab24ea4388e6321d497ca007de1
vasvince98 <87307436+vasvince98@users.noreply.github.com>
2024-02-06 20:08:36 +0100

    refactored to python

--- Tanmenet/1.session/first_session.py
+++ Tanmenet/1.session/first_session.py
@@ -3,34 +3,31 @@ INDENT = "#######################################"
 
 # 1. feladat: String használat
 print("Így tudunk üzenetet kiírni.")
-
 message = "Így pedig változón keresztül írhatunk ki valamit."
 print(message)
 
-# Konkatenáció
-firstString = "Így lehet összekötni"
-secondString = "két darab string-et."
 
-print(firstString, secondString)
+# Konkatenáció
+string1 = "Így lehet összekötni"
+string2 = "két darab string-et."
+print(string1, string2)
 
 
 # 2. feladat: int-ek
 print(2 + 3)
-
 number = 2
 number2 = 6
 print(number + number2)
 
 # Vegyes konkatenáció
-
-print(firstString, 2, secondString)
-print(firstString, number, secondString)
+print(string1, 2, string2)
+print(string1, number, string2)
 
 # Átláthatóság kedvéért: String interpoláció
 
-stringToBeInserted = "szöveget"
-numberToBeInserted = 5
-print(f"Beírok ide valamennyi {stringToBeInserted} és egy számot is: {numberToBeInserted}.")
+string_to_be_inserted = "szöveget"
+number_to_be_inserted = 5
+print(f"Beírok ide valamennyi {string_to_be_inserted} és egy számot is: {number_to_be_inserted}.")
 
 # Mi lenne ha ezt szeretnénk sokszor kiírni?
 # Újra meg újra le kéne írni ezeket. Függvények bevezetése
@@ -63,21 +60,36 @@ def summary(first_number, second_number):
 print(summary(10, 2))
 
 # 4.b újra írni mindent amit eddig csináltunk fgv-ként, innentől mindent függvényként csinálunk.
+# Először definiálom a függvényt -> nem történik semmi. Miért? -> meghívom a függvényt
+
 
 def first_exercise():
-    print("Ez egy üzenet a függvényen belül")
-    message = "Ez egy változóba mentett üzenet a függvényen belül"
-    print(message)
+    print("Első feladat:")
+    print("Így tudunk üzenetet kiírni.")
+
+    first_message = "Így pedig változón keresztül írhatunk ki valamit."
+    print(first_message)
+
+    # Konkatenáció
+    first_string = "Így lehet összekötni"
+    first_string2 = "két darab string-et."
+
+    print(first_string + " " + first_string2)
+
+
+def second_exercise():
+    print("Második feladat:")
     print(2 + 3)
 
-    number = 10
-    number2 = 12
-    print(number + number2)
+    second_number = 2
+    second_number2 = 6
+    print(second_number + second_number2)
 
 
 print(INDENT)
 first_exercise()
-# second_exercise()
+print(INDENT)
+second_exercise()
 print(INDENT)
 
 
