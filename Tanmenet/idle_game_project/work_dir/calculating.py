ff889e4939b9d7003fd552e8dbb096462a50f075
Borisz42 <borbely.david@gmail.com>
2024-08-04 15:02:09 +0200

    Small modifications

--- Tanmenet/idle_game_project/work_dir/calculating.py
+++ Tanmenet/idle_game_project/work_dir/calculating.py
@@ -1,5 +1,11 @@
 import random
 from datetime import datetime
+import os
+
+
+def cls():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
 
 coins = 0
 COIN_REWARD = 1
@@ -8,7 +14,9 @@ abacuses = 0
 brainPowers = 0
 calculators = 0
 desktops = 0
-electricity = 0
+happiness = 0
+
+HAPPINESS_PRICE = 100000
 
 ABAC_YIELD = 1
 ABAC_PRICE = 2
@@ -27,16 +35,18 @@ def printMenuAndRiddle():
     a = random.randrange(0, 10, 1)
     b = random.randrange(0, 10, 1)
     print(f''' 
-    You have {coins} coins!
-    Solve this for {COIN_REWARD} Coin: {a} x {b} = ? 
-    Type your answer or you can type one letter to do the following actions: 
-    a - Buy Abacus ({ABAC_YIELD} Coins/sec) for {ABAC_PRICE} Coin 
-    b - Buy Brain ({BRAIN_YIELD} Coins/sec) power for {BRAIN_PRICE} Coin
-    c - Buy Calculator ({CALC_YIELD} Coins/sec) for {CALC_PRICE} Coin
-    d - Buy Desktop ({DESKTOP_YIELD} Coins/sec) computer for {DESKTOP_PRICE} Coin
-    e - Buy 1 unit of Eternal happiness (it is very good) for 100 000 Coin
-    p - Check inventory
-    q - Quit''')
+----------------------------------------------------------------------------------
+    You have {coins} coins! 
+    You are making {abacuses * ABAC_YIELD + brainPowers * BRAIN_YIELD + calculators * CALC_YIELD + desktops * DESKTOP_YIELD} Coins/sec
+    Menu options: 
+    a - Buy Abacus ({ABAC_YIELD} Coins/sec) for {real_price(ABAC_PRICE, abacuses)} Coin 
+    b - Buy Brain ({BRAIN_YIELD} Coins/sec) power for {real_price(BRAIN_PRICE, brainPowers)} Coin
+    c - Buy Calculator ({CALC_YIELD} Coins/sec) for {real_price(CALC_PRICE, calculators)} Coin
+    d - Buy Desktop ({DESKTOP_YIELD} Coins/sec) computer for {real_price(DESKTOP_PRICE, desktops)} Coin
+    e - Buy 1 unit of Eternal happiness (it is very good) for {real_price(HAPPINESS_PRICE, happiness)} Coin
+    i - Check inventory
+    q - Quit
+Solve this for {COIN_REWARD} Coin: {a} x {b} = ?''')
     return a * b
 
 
@@ -49,9 +59,24 @@ def stats():
     {brainPowers} Brain power which makes {brainPowers * BRAIN_YIELD} Coins/sec
     {calculators} Calculator which makes {calculators * CALC_YIELD} Coins/sec 
     {desktops} Desktop which makes {desktops * DESKTOP_YIELD} Coins/sec 
-    {electricity} unit of Eternal happiness which is very good 
+    {happiness} unit of Eternal happiness which is very good 
     
     In total you are making {abacuses * ABAC_YIELD + brainPowers * BRAIN_YIELD + calculators * CALC_YIELD + desktops * DESKTOP_YIELD} Coins/sec ''')
+
+
+def real_price(basePrice, itemCount):
+    return round((1 + itemCount) * basePrice * 1.1)
+
+
+def buy_item(itemName, itemVar, price, coinsVar):
+    realPrice = real_price(price, itemVar)
+    if coinsVar >= realPrice:
+        coinsVar -= realPrice
+        itemVar += 1
+        print(f"1 {itemName} added!")
+    else:
+        print("You don't have enough coins to buy this :/")
+    return itemVar, coinsVar
 
 
 userInput = 1
@@ -60,57 +85,31 @@ while userInput != 'q':
     result = printMenuAndRiddle()
 
     time1 = datetime.now()
-    userInput = input()
+    userInput = input("Type your answer or choose a menu point: ")
     time2 = datetime.now()
 
     timeDiff = (time2 - time1).seconds
     coins += timeDiff * (
-                abacuses * ABAC_YIELD + brainPowers * BRAIN_YIELD + calculators * CALC_YIELD + desktops * DESKTOP_YIELD)
+            abacuses * ABAC_YIELD + brainPowers * BRAIN_YIELD + calculators * CALC_YIELD + desktops * DESKTOP_YIELD)
     if userInput.isnumeric():
         if int(userInput) == result:
             print(f"Great! Your reward is {COIN_REWARD} coin!")
             coins += 1
         else:
             print(f"Wrong answer! It was actually {result}, You don't get any coin :/")
-        print(f"Total coins: {coins}")
     else:
         match userInput:
             case "a":
-                if coins >= ABAC_PRICE:
-                    coins -= ABAC_PRICE
-                    abacuses += 1
-                    print(f"1 Abacus added! Now you are making {abacuses * ABAC_YIELD + brainPowers * BRAIN_YIELD + calculators * CALC_YIELD + desktops * DESKTOP_YIELD} Coins/sec")
-                else:
-                    print("You don't have enough coins to buy this :/")
+                abacuses, coins = buy_item("Abacus", abacuses, ABAC_PRICE, coins)
             case "b":
-                if coins >= BRAIN_PRICE:
-                    coins -= BRAIN_PRICE
-                    brainPowers += 1
-                    print(f"1 Brain power added! Now you are making {abacuses * ABAC_YIELD + brainPowers * BRAIN_YIELD + calculators * CALC_YIELD + desktops * DESKTOP_YIELD} Coins/sec")
-                else:
-                    print("You don't have enough coins to buy this :/")
+                brainPowers, coins = buy_item("Brain power", brainPowers, BRAIN_PRICE, coins)
             case "c":
-                if coins >= CALC_PRICE:
-                    coins -= CALC_PRICE
-                    calculators += 1
-                    print(f"1 Calculator added! Now you are making {abacuses * ABAC_YIELD + brainPowers * BRAIN_YIELD + calculators * CALC_YIELD + desktops * DESKTOP_YIELD} Coins/sec")
-                else:
-                    print("You don't have enough coins to buy this :/")
+                calculators, coins = buy_item("Calculator", calculators, CALC_PRICE, coins)
             case "d":
-                if coins >= DESKTOP_PRICE:
-                    coins -= DESKTOP_PRICE
-                    desktops += 1
-                    print(f"1 Desktop computer added! Now you are making {abacuses * ABAC_YIELD + brainPowers * BRAIN_YIELD + calculators * CALC_YIELD + desktops * DESKTOP_YIELD} Coins/sec")
-                else:
-                    print("You don't have enough coins to buy this :/")
+                desktops, coins = buy_item("Desktop computer", desktops, DESKTOP_PRICE, coins)
             case "e":
-                if coins >= 100000:
-                    coins -= 100000
-                    electricity += 1
-                    print(f"1 unit of Eternal happiness added! That feels very good!")
-                else:
-                    print("You don't have enough coins to buy this :/")
-            case "p":
+                happiness, coins = buy_item("unit of Eternal happiness", happiness, HAPPINESS_PRICE, coins)
+            case "i":
                 stats()
             case "q":
                 print("Exiting the application...")
